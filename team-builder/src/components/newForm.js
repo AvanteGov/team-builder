223d419f476703706518c7581868e32e39942e44
Alex <alexcouts18@gmail.com>
2020-03-21 00:35:27 -0400

    added styling to team page and form page components

--- team-builder/src/components/newForm.js
+++ team-builder/src/components/newForm.js
@@ -2,31 +2,36 @@ import React, { useState } from "react";
 
 const NewForm = (props) => {
 
+    // sets up state to track form input and manage blank form state
     const [userInput, setUserInput] = useState({
         name: "",
         email: "",
         title: ""
     })
 
+    // on change event extends the state object to create a new object 
     const changeHandler = (event) => {
         setUserInput({ ...userInput, [event.target.name]: event.target.value });
     }
 
+    // handles submission by bringing in handler function from App.js 
+    // and creating function to invoke it 
     const submitHandler = (event) => {
         event.preventDefault()
+        // calling the function from props, and setting unique id with Date
         props.addMember({
             ...userInput,
             id: Date.now()
         })
     }
 
-    console.log(userInput);
+    // console.log(userInput);
 
     return (
         <form onSubmit={submitHandler} className="user-form">
             <label htmlFor="name-id" className="user-form__label">Full Name</label>
             <input 
-                className="user-form__name-input"
+                className="user-form__input"
                 placeholder="Full Name"
                 type="text"
                 id="name-id"
@@ -37,7 +42,7 @@ const NewForm = (props) => {
             
             <label htmlFor="email-id" className="user-form__label">Email</label>
             <input 
-                className="user-form__email-input"
+                className="user-form__input"
                 placeholder="Email"
                 type="text"
                 id="email-id"
@@ -48,7 +53,7 @@ const NewForm = (props) => {
             
             <label htmlFor="title-id" className="user-form__label">Position Title</label>
             <input 
-                className="user-form__title-input"
+                className="user-form__input"
                 placeholder="Position Title"
                 type="text"
                 id="title-id"
